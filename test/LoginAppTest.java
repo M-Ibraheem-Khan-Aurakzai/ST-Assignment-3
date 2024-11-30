0703cca88ab034c1d30cd2bd2fb9c29fc79f112b
M-Ibraheem-Khan-Aurakzai <mikaurakzai@gmail.com>
2024-11-30 20:11:01 +0500

    Update LoginAppTest.java

--- test/LoginAppTest.java
+++ test/LoginAppTest.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class LoginAppTest {
+public class LoginAppTest {
     //Test cases for Email Authentication
     @org.junit.jupiter.api.Test //Testing that a valid email existing in database returns the valid name of the email
     void testValidEmail() {
